4f1f2457ac7075cf7bdd29b54940b671dc84505e
Sulivan7 <Devsulivan@gmail.com>
2026-04-26 17:42:02 -0300

    Update project data

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,3 +5,12 @@ export interface Skill {
   title: string;
   description: string;
 }
+
+export interface Project {
+  image: string;
+  title: string;
+  description: string;
+  tags: string[];
+  githubLink: string;
+  liveLink?: string;
+}
